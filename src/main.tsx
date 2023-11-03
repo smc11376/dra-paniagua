027b2d73f8b85c7302f0edd1464765820d96459a
sergio.magarino <sergio.magarino@intelygenz.com>
2023-11-03 21:53:54 +0100

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
