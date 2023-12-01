3f62fabece683d9f963bd1957073b0e1b35449bb
sergio <s.magarino@gmail.com>
2023-12-01 17:37:45 +0100

    feat: images

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import {Helmet, HelmetProvider} from "react-helmet-async";
 import {Link} from "react-router-dom";
 import Typewriter from "typewriter-effect";
 import "./style.css";
+import ImgEscritorio from './escritorio.jpg'
 
 export const Home = () => {
   return (
@@ -13,11 +14,10 @@ export const Home = () => {
           <title> {meta.title}</title>
           <meta name="description" content={meta.description} />
         </Helmet>
-        <div className="intro_sec d-block d-lg-flex align-items-center ">
-          <div
-            className="h_bg-image order-1 order-lg-2 h-100 "
-            style={{ backgroundImage: `url(src/assets/images/escritorio.jpg)` }}
-          ></div>
+        <div className="intro_sec d-block d-lg-flex align-items-center">
+          <div className="h_bg-image order-1 order-lg-2 h-100">
+            <img src={ImgEscritorio} alt="Main" />
+          </div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
               <div className="intro mx-auto">
